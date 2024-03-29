389ae566a78a4e075331009c8a1e9505a4851443
Parkenew <126756270+Parkenew@users.noreply.github.com>
2024-03-29 13:08:52 +0900

    Delete test2 directory

--- test2/src/main/java/com/example/test2/DataNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.test2;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-public class DataNotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
